fd1fbf6df55b92b28a333a4ca7abd4dc29194c86
Mikael Kvist <co17de11@gmail.com>
2023-08-31 22:12:09 +0200

    feat(state): add internal state as a package

---
+++ packages/state/lib/src/object/anime_response_intern.dart
@@ -0,0 +1,4 @@
+import 'package:jikan_api/jikan_api.dart';
+import 'anime_intern.dart';
+
+abstract class AnimeResponseIntern extends Response<List<AnimeIntern>> {}
---
+++ packages/state/lib/state.dart
@@ -0,0 +1,10 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/object/anime_intern.dart';
+export 'src/object/anime_response_intern.dart';
+export 'src/object/producer_intern.dart';
+
+// TODO: Export any libraries intended for clients of this package.
