9361c2e559541bb7655a59bedb52c9cf810dd880
Mikael Kvist <>
2023-09-02 20:21:30 +0200

    feat(database): add functions to database interface

--- packages/database/lib/src/database_base.dart
+++ packages/database/lib/src/database_base.dart
@@ -3,7 +3,17 @@ import 'package:database/src/model.dart';
 import 'stub/database.dart' if (dart.library.io) 'isar/database.dart';
 
 abstract interface class Database implements ModelProxy {
+  /// Initialise the database
   Future<void> init();
+
+  /// Clear data
+  Future<void> clear();
+
+  /// Close the database connection
+  Future<void> close();
+
+  /// Remove all data including the database-files
+  Future<void> remove();
 
   factory Database() => database;
 }
