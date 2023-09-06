2d8280633d2bc4a6fed5f416189687719cfe8b1e
Mikael Kvist <co17de11@gmail.com>
2023-09-06 20:16:47 +0200

    feat(ui): add tabview to home page

--- packages/ui/lib/src/home.dart
+++ packages/ui/lib/src/home.dart
@@ -10,8 +10,34 @@ class Home extends ConsumerStatefulWidget {
 class _HomeState extends ConsumerState<Home> {
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      child: Text("YOOO"),
+    if (!mounted) {
+      return const SizedBox(
+        child: const Text("Not mounted"),
+      );
+    }
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        extendBody: true,
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.favorite)),
+              Tab(icon: Icon(Icons.home)),
+              Tab(icon: Icon(Icons.schedule)),
+            ],
+          ),
+          toolbarHeight: 0,
+        ),
+        body: const TabBarView(
+          children: [
+            SizedBox(child: Text("Yo1")),
+            SizedBox(child: Text("Yo2")),
+            SizedBox(child: Text("Yo3")),
+          ],
+        ),
+      ),
     );
   }
 }
