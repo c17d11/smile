0b2ce361767c88853bd44b9a4b6b822995abd93d
Mikael Kvist <co17de11@gmail.com>
2023-09-06 19:46:53 +0200

    feat(ui): add empty home page

--- packages/ui/lib/main.dart
+++ packages/ui/lib/main.dart
@@ -1,15 +1,37 @@
-import 'package:flutter/material.dart';\
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:database/database.dart';
+import 'package:jikan_api/jikan_api.dart';
+import 'package:ui/src/home.dart';
 
-void main() {
-  runApp(JikanUi());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Database db = Database();
+  await db.init();
+  JikanApi api = JikanApi();
+
+  runApp(JikanUi(db, api));
 }
 
 class JikanUi extends ConsumerStatefulWidget {
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
+  final Database _db;
+  final JikanApi _api;
 
+  const JikanUi(this._db, this._api, {super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _JikanUiState();
+}
+
+class _JikanUiState extends ConsumerState<JikanUi> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => Home(),
+      },
+    );
+  }
 }
---
+++ packages/ui/lib/src/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Home extends ConsumerStatefulWidget {
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends ConsumerState<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      child: Text("YOOO"),
+    );
+  }
+}
