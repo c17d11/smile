3b03fa93aeec414e47e60398b392ac3ec5fd44dd
Mikael Kvist <>
2023-09-06 19:24:51 +0200

    feat(state): add anime controller

---
+++ packages/state/lib/src/controller/anime_controller.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jikan_api/jikan_api.dart';
+import 'package:state/state.dart';
+import 'package:database/database.dart';
+
+class AnimeController extends StateNotifier<AsyncValue<List<AnimeIntern>>> {
+  Database _database;
+  JikanApi _api;
+
+  AnimeController(this._database, this._api) : super(AsyncValue.data([]));
+
+  Future<AnimeResponseIntern?> _getDatabaseResponse(AnimeQuery query) async {
+    AnimeResponseIntern? res = await _database.getAnimeResponse(query);
+    return res;
+  }
+
+  Future<AnimeResponseIntern> _getApiResponse(AnimeQuery query) async {
+    AnimeResponse res = await _api.searchAnimes(query);
+
+    // TODO: move this function to 'state'
+    AnimeResponseIntern resIntern =
+        _database.createAnimeResponseIntern(res, query);
+    return resIntern;
+  }
+
+  Future<void> _storeResponse(AnimeResponseIntern res) async {
+    await _database.insertAnimeResponse(res);
+  }
+
+  Future<AnimeResponseIntern> _getResponse(AnimeQuery query) async {
+    AnimeResponseIntern? res = await _getDatabaseResponse(query);
+    if (res == null) {
+      res = await _getApiResponse(query);
+      await _storeResponse(res);
+    }
+    return res;
+  }
+
+  Future<void> get(AnimeQuery query) async {
+    try {
+      state = const AsyncLoading();
+      AnimeResponseIntern res = await _getResponse(query);
+
+      List<AnimeIntern> animes = res.data ?? [];
+      state = AsyncValue.data(animes);
+    } on JikanApiException catch (e, stacktrace) {
+      state = AsyncError(e, stacktrace);
+
+      // TODO: Database error
+    }
+  }
+}
