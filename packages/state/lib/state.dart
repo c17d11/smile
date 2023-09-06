4eb4c9f4a2fea8f68a729285a26a3340313f194d
Mikael Kvist <co17de11@gmail.com>
2023-09-06 21:23:30 +0200

    fix(state): expose anime controller

--- packages/state/lib/state.dart
+++ packages/state/lib/state.dart
@@ -6,5 +6,6 @@ library;
 export 'src/object/anime_intern.dart';
 export 'src/object/anime_response_intern.dart';
 export 'src/object/producer_intern.dart';
+export 'src/controller/anime_controller.dart';
 
 // TODO: Export any libraries intended for clients of this package.
