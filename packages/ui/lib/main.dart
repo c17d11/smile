9c62dddc43e218063ffd4eed1b361df8a06616c4
Mikael Kvist <>
2023-09-09 18:02:10 +0200

    feat(ui): add navigation bar, navigation rail and auto-hiding filter bar

--- packages/ui/lib/main.dart
+++ packages/ui/lib/main.dart
@@ -6,17 +6,17 @@ import 'package:ui/src/pod.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const ProviderScope(child: JikanUi()));
+  runApp(const ProviderScope(child: App()));
 }
 
-class JikanUi extends ConsumerStatefulWidget {
-  const JikanUi({super.key});
+class App extends ConsumerStatefulWidget {
+  const App({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _JikanUiState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _AppState();
 }
 
-class _JikanUiState extends ConsumerState<JikanUi> {
+class _AppState extends ConsumerState<App> {
   Future<void> init() async {
     await ref.watch(databasePod).init();
   }
@@ -28,8 +28,12 @@ class _JikanUiState extends ConsumerState<JikanUi> {
         builder: (context, snapshot) {
           return MaterialApp(
             initialRoute: 'home',
+            theme: ThemeData(
+                colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
             routes: {
-              'home': (context) => const Home(),
+              'home': (context) => const HomePage(),
+              'anime-details': (context) => const HomePage(),
+              'anime-query': (context) => const HomePage(),
             },
           );
         });
--- packages/ui/lib/src/home.dart
+++ packages/ui/lib/src/home.dart
@@ -2,43 +2,140 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ui/src/anime_list.dart';
 
-class Home extends ConsumerStatefulWidget {
-  const Home({super.key});
+class HomePage extends ConsumerStatefulWidget {
+  const HomePage({super.key});
+
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _HomeState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomePageState();
 }
 
-class _HomeState extends ConsumerState<Home> {
+class _HomePageState extends ConsumerState<HomePage> {
+  int currentPageIndex = 0;
+
+  Widget buildNavigationBar() {
+    return NavigationBar(
+      labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+      onDestinationSelected: (int index) {
+        setState(() {
+          currentPageIndex = index;
+        });
+      },
+      selectedIndex: currentPageIndex,
+      destinations: const <Widget>[
+        NavigationDestination(
+          selectedIcon: Icon(Icons.new_releases_outlined),
+          icon: Icon(Icons.new_releases),
+          label: 'News',
+        ),
+        NavigationDestination(
+          selectedIcon: Icon(Icons.favorite_outline),
+          icon: Icon(Icons.favorite),
+          label: 'Favorite',
+        ),
+        NavigationDestination(
+          selectedIcon: Icon(Icons.home_outlined),
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        NavigationDestination(
+          selectedIcon: Icon(Icons.calendar_month_outlined),
+          icon: Icon(Icons.calendar_month),
+          label: 'Schedule',
+        ),
+        NavigationDestination(
+          selectedIcon: Icon(Icons.settings_outlined),
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+      ],
+    );
+  }
+
+  Widget buildNavigationRail() {
+    return NavigationRail(
+      selectedIndex: currentPageIndex,
+      groupAlignment: -0.95,
+      labelType: NavigationRailLabelType.all,
+      onDestinationSelected: (int index) {
+        setState(() {
+          currentPageIndex = index;
+        });
+      },
+      destinations: const <NavigationRailDestination>[
+        NavigationRailDestination(
+          selectedIcon: Icon(Icons.new_releases_outlined),
+          icon: Icon(Icons.new_releases),
+          label: Text('News'),
+        ),
+        NavigationRailDestination(
+          selectedIcon: Icon(Icons.favorite_outline),
+          icon: Icon(Icons.favorite),
+          label: Text('Favorite'),
+        ),
+        NavigationRailDestination(
+          selectedIcon: Icon(Icons.home_outlined),
+          icon: Icon(Icons.home),
+          label: Text('Home'),
+        ),
+        NavigationRailDestination(
+          selectedIcon: Icon(Icons.calendar_month_outlined),
+          icon: Icon(Icons.calendar_month),
+          label: Text('Schedule'),
+        ),
+        NavigationRailDestination(
+          selectedIcon: Icon(Icons.settings_outlined),
+          icon: Icon(Icons.settings),
+          label: Text('Settings'),
+        ),
+      ],
+    );
+  }
+
+  Widget buildBody() {
+    return <Widget>[
+      Container(
+        color: Colors.red,
+        alignment: Alignment.center,
+        child: const Text('Page 1'),
+      ),
+      Container(
+        color: Colors.green,
+        alignment: Alignment.center,
+        child: const Text('Page 2'),
+      ),
+      const AnimeList(),
+      Container(
+        color: Colors.purple,
+        alignment: Alignment.center,
+        child: const Text('Page 4'),
+      ),
+      Container(
+        color: Colors.orange,
+        alignment: Alignment.center,
+        child: const Text('Page 5'),
+      ),
+    ][currentPageIndex];
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (!mounted) {
-      return const SizedBox(
-        child: Text("Not mounted"),
-      );
-    }
-    return DefaultTabController(
-      initialIndex: 1,
-      length: 3,
-      child: Scaffold(
-        extendBody: true,
-        appBar: AppBar(
-          bottom: const TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.favorite)),
-              Tab(icon: Icon(Icons.home)),
-              Tab(icon: Icon(Icons.schedule)),
-            ],
-          ),
-          toolbarHeight: 0,
-        ),
-        body: const TabBarView(
-          children: [
-            SizedBox(child: Text("Yo1")),
-            AnimeList(),
-            SizedBox(child: Text("Yo3")),
-          ],
-        ),
-      ),
+    bool isWideScreen = MediaQuery.of(context).size.aspectRatio > 1;
+
+    return Scaffold(
+      bottomNavigationBar: isWideScreen ? null : buildNavigationBar(),
+      body: isWideScreen
+          ? SafeArea(
+              child: Row(
+                children: <Widget>[
+                  buildNavigationRail(),
+                  const VerticalDivider(thickness: 1, width: 1),
+                  Expanded(
+                    child: buildBody(),
+                  ),
+                ],
+              ),
+            )
+          : buildBody(),
     );
   }
 }
