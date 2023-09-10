a280be03e74f93f25a6a1c38c3a1d82e013ba360
Mikael Kvist <co17de11@gmail.com>
2023-09-10 17:16:17 +0200

    feat(ui): use navigation_container package

--- packages/ui/lib/main.dart
+++ packages/ui/lib/main.dart
@@ -29,7 +29,20 @@ class _AppState extends ConsumerState<App> {
           return MaterialApp(
             initialRoute: 'home',
             theme: ThemeData(
-                colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
+              colorSchemeSeed: const Color(0xff6750a4),
+              useMaterial3: true,
+
+              // IconButton theme mimicing material3 NavigationBar buttons
+              iconButtonTheme: IconButtonThemeData(
+                style: IconButton.styleFrom(
+                  minimumSize: const Size(50, 20),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100)),
+                ),
+              ),
+            ),
             routes: {
               'home': (context) => const HomePage(),
               'anime-details': (context) => const HomePage(),
--- packages/ui/lib/src/home.dart
+++ packages/ui/lib/src/home.dart
@@ -1,159 +1,130 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:navigation_container/navigation_container.dart';
 import 'package:ui/src/anime_list.dart';
 
-class HomePage extends ConsumerStatefulWidget {
+class HomeNavItem extends IconItem {
+  @override
+  String get label => "Home";
+
+  @override
+  Icon get icon => const Icon(Icons.home_outlined);
+
+  @override
+  Icon get selectedIcon => const Icon(Icons.home);
+
+  @override
+  Widget buildContent() {
+    return const AnimeList();
+  }
+}
+
+class FavoriteNavItem extends IconItem {
+  @override
+  String get label => "Favorite";
+
+  @override
+  Icon get icon => const Icon(Icons.favorite_outline);
+
+  @override
+  Icon get selectedIcon => const Icon(Icons.favorite);
+
+  @override
+  Widget buildContent() {
+    return Container(
+      color: Colors.green[300],
+    );
+  }
+}
+
+class ScheduleNavItem extends IconItem {
+  @override
+  String get label => "Schedule";
+
+  @override
+  Icon get icon => const Icon(Icons.calendar_month_outlined);
+
+  @override
+  Icon get selectedIcon => const Icon(Icons.calendar_month);
+
+  @override
+  Widget buildContent() {
+    return Container(
+      color: Colors.red[300],
+    );
+  }
+}
+
+class NewsNavItem extends IconItem {
+  @override
+  String get label => "News";
+
+  @override
+  Icon get icon => const Icon(Icons.new_releases_outlined);
+
+  @override
+  Icon get selectedIcon => const Icon(Icons.new_releases);
+
+  @override
+  Widget buildContent() {
+    return Container(
+      color: Colors.yellow[300],
+    );
+  }
+}
+
+class SettingsNavItem extends IconItem {
+  @override
+  String get label => "Settings";
+
+  @override
+  Icon get icon => const Icon(Icons.settings_outlined);
+
+  @override
+  Icon get selectedIcon => const Icon(Icons.settings);
+
+  @override
+  Widget buildContent() {
+    return Container(
+      color: Colors.purple[300],
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _HomePageState();
+  State<StatefulWidget> createState() => _HomePageState();
 }
 
-class _HomePageState extends ConsumerState<HomePage> {
-  int currentPageIndex = 0;
-
-  Widget buildNavigationBar() {
-    return NavigationBar(
-      labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-      onDestinationSelected: (int index) {
-        setState(() {
-          currentPageIndex = index;
-        });
-      },
-      selectedIndex: currentPageIndex,
-      destinations: <Widget>[
-        _NewsItem().navItem,
-        _FavoriteItem().navItem,
-        _HomeItem().navItem,
-        _ScheduleItem().navItem,
-        _SettingsItem().navItem,
-      ],
-    );
-  }
-
-  Widget buildNavigationRail() {
-    return NavigationRail(
-      selectedIndex: currentPageIndex,
-      groupAlignment: -0.95,
-      labelType: NavigationRailLabelType.all,
-      onDestinationSelected: (int index) {
-        setState(() {
-          currentPageIndex = index;
-        });
-      },
-      destinations: <NavigationRailDestination>[
-        _NewsItem().railItem,
-        _FavoriteItem().railItem,
-        _HomeItem().railItem,
-        _ScheduleItem().railItem,
-        _SettingsItem().railItem,
-      ],
-    );
-  }
-
-  Widget buildBody() {
-    return <Widget>[
-      Container(
-        color: Colors.red,
-        alignment: Alignment.center,
-        child: const Text('Page 1'),
-      ),
-      Container(
-        color: Colors.green,
-        alignment: Alignment.center,
-        child: const Text('Page 2'),
-      ),
-      const AnimeList(),
-      Container(
-        color: Colors.purple,
-        alignment: Alignment.center,
-        child: const Text('Page 4'),
-      ),
-      Container(
-        color: Colors.orange,
-        alignment: Alignment.center,
-        child: const Text('Page 5'),
-      ),
-    ][currentPageIndex];
-  }
+class _HomePageState extends State<HomePage> {
+  IconItem selectedContent = HomeNavItem();
 
   @override
   Widget build(BuildContext context) {
-    bool isWideScreen = MediaQuery.of(context).size.aspectRatio > 1;
-
-    return Scaffold(
-      bottomNavigationBar: isWideScreen ? null : buildNavigationBar(),
-      body: isWideScreen
-          ? SafeArea(
-              child: Row(
-                children: <Widget>[
-                  buildNavigationRail(),
-                  const VerticalDivider(thickness: 1, width: 1),
-                  Expanded(
-                    child: buildBody(),
-                  ),
-                ],
-              ),
-            )
-          : buildBody(),
+    return NavigationContainer(
+      startItem: selectedContent,
+      landscapeItems: [
+        NewsNavItem(),
+        FavoriteNavItem(),
+        HomeNavItem(),
+        ScheduleNavItem(),
+        SettingsNavItem(),
+      ],
+      portraitItems: [
+        HomeNavItem(),
+        FavoriteNavItem(),
+        ScheduleNavItem(),
+        NewsNavItem(),
+        SpaceItem(),
+        SettingsNavItem(),
+      ],
+      content: selectedContent.buildContent(),
+      onClick: (IconItem selected) => setState(
+        () {
+          selectedContent = selected;
+        },
+      ),
     );
-  }
-}
-
-abstract class _PageItem {
-  late Icon iconSelected;
-  late Icon icon;
-  late String name;
-
-  NavigationDestination get navItem => NavigationDestination(
-        selectedIcon: iconSelected,
-        icon: icon,
-        label: name,
-      );
-
-  NavigationRailDestination get railItem => NavigationRailDestination(
-        selectedIcon: iconSelected,
-        icon: icon,
-        label: Text(name),
-      );
-}
-
-class _HomeItem extends _PageItem {
-  _HomeItem() {
-    iconSelected = const Icon(Icons.home_outlined);
-    icon = const Icon(Icons.home);
-    name = "Home";
-  }
-}
-
-class _NewsItem extends _PageItem {
-  _NewsItem() {
-    iconSelected = const Icon(Icons.new_releases_outlined);
-    icon = const Icon(Icons.new_releases);
-    name = "News";
-  }
-}
-
-class _FavoriteItem extends _PageItem {
-  _FavoriteItem() {
-    iconSelected = const Icon(Icons.favorite_outline);
-    icon = const Icon(Icons.favorite);
-    name = "Favorite";
-  }
-}
-
-class _ScheduleItem extends _PageItem {
-  _ScheduleItem() {
-    iconSelected = const Icon(Icons.calendar_month_outlined);
-    icon = const Icon(Icons.calendar_month);
-    name = "Schedule";
-  }
-}
-
-class _SettingsItem extends _PageItem {
-  _SettingsItem() {
-    iconSelected = const Icon(Icons.settings_outlined);
-    icon = const Icon(Icons.settings);
-    name = "Settings";
   }
 }
