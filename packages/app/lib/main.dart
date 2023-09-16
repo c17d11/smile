539502d877513971bdd5b6f13034d30f4c341c59
Mikael Kvist <>
2023-09-16 18:32:25 +0200

    feat(app): create main package

---
+++ packages/app/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:app/ui/src/anime_details.dart';
+import 'package:app/ui/src/anime_query_page.dart';
+import 'package:app/ui/src/home.dart';
+import 'package:app/ui/src/pod.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const ProviderScope(child: App()));
+}
+
+class App extends ConsumerStatefulWidget {
+  const App({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _AppState();
+}
+
+class _AppState extends ConsumerState<App> {
+  @override
+  Widget build(BuildContext context) {
+    final init = ref.watch(initPod);
+
+    return init.when(
+      data: (initSuccessful) => MaterialApp(
+        theme: ThemeData(
+          colorSchemeSeed: const Color(0xff6750a4),
+          useMaterial3: true,
+
+          // IconButton theme mimicing material3 NavigationBar buttons
+          iconButtonTheme: IconButtonThemeData(
+            style: IconButton.styleFrom(
+              minimumSize: const Size(50, 20),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100)),
+            ),
+          ),
+        ),
+        initialRoute: 'home',
+        routes: {
+          'home': (context) => const HomePage(),
+          'anime-details': (context) => const AnimeDetails(),
+          'anime-query': (context) => const AnimeQueryPage(),
+        },
+      ),
+      error: (err, stack) => Text('Error: $err'),
+      loading: () => const CircularProgressIndicator(),
+    );
+  }
+}
