3f6294f1b36a42c9eb2c39a256c26144d91e731a
Mikael Kvist <>
2023-09-16 16:03:21 +0200

    refactor(navigation_controller): make navigation controller a librariy in ui instead of package

--- packages/app/lib/ui/navigation_container/src/icon_item.dart
+++ packages/app/lib/ui/navigation_container/src/icon_item.dart
@@ -1,6 +1,6 @@
+import 'package:app/ui/navigation_container/src/animated_nav_item.dart';
+import 'package:app/ui/navigation_container/src/nav_item.dart';
 import 'package:flutter/material.dart';
-import 'package:navigation_container/src/animated_nav_item.dart';
-import 'package:navigation_container/src/nav_item.dart';
 
 abstract class IconItem extends NavItem {
   String get label;
--- packages/app/lib/ui/navigation_container/src/navigation_container.dart
+++ packages/app/lib/ui/navigation_container/src/navigation_container.dart
@@ -1,7 +1,7 @@
+import 'package:app/ui/navigation_container/navigation_container.dart';
+import 'package:app/ui/navigation_container/src/nav_bar.dart';
+import 'package:app/ui/navigation_container/src/nav_item.dart';
 import 'package:flutter/material.dart';
-import 'package:navigation_container/src/icon_item.dart';
-import 'package:navigation_container/src/nav_bar.dart';
-import 'package:navigation_container/src/nav_item.dart';
 
 class NavigationContainer extends StatelessWidget {
   final NavItem startItem;
