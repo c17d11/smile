54ab7ab1a7084d41292c875f7fad5a1a64973a9e
Mikael Kvist <co17de11@gmail.com>
2023-10-24 18:39:40 +0200

    fix(ui): expanded spacer error

--- packages/app/lib/ui/navigation_container/src/space_item.dart
+++ packages/app/lib/ui/navigation_container/src/space_item.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 
 class SpaceItem extends NavItem {
   @override
-  Widget build(bool isSelected, void Function()? onPressed) =>
-      const Expanded(child: Spacer());
+  Widget build(bool isSelected, void Function()? onPressed) => const Spacer();
 
   @override
   String get id => '';
