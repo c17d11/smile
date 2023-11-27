0a457c03dd2dbeada122835065178ce133e5557d
Mikael Kvist <>
2023-11-27 20:09:06 +0100

    feat: genre page

--- packages/app/lib/controller/src/controller/genre_controller.dart
+++ packages/app/lib/controller/src/controller/genre_controller.dart
@@ -5,16 +5,28 @@ import 'package:jikan_api/jikan_api.dart';
 
 class GenreController extends StateNotifier<AsyncValue<List<GenreIntern>>> {
   final Database _database;
+  final JikanApi _api;
 
-  GenreController(this._database) : super(const AsyncValue.data([]));
+  GenreController(this._database, this._api) : super(const AsyncValue.data([]));
 
   Future<List<GenreIntern>> _getDatabaseGenres() async {
     List<GenreIntern> genres = await _database.getAllGenres();
     return genres;
   }
 
+  Future<List<Genre>> _getApiGenres() async {
+    List<Genre> genres = await _api.searchGenres();
+    return genres;
+  }
+
   Future<List<GenreIntern>> _getGenres() async {
     List<GenreIntern> genres = await _getDatabaseGenres();
+    if (genres.isEmpty) {
+      List<Genre> apiGenres = await _getApiGenres();
+      await _database.insertGenres(apiGenres);
+    }
+    genres = await _getDatabaseGenres();
+    genres.sort((a, b) => (a.name ?? "").compareTo(b.name ?? ""));
     return genres;
   }
 
---
+++ packages/app/lib/ui/src/genre_list.dart
@@ -0,0 +1,62 @@
+import 'package:app/controller/src/object/anime_query_intern.dart';
+import 'package:app/controller/src/object/genre_intern.dart';
+import 'package:app/ui/src/home.dart';
+import 'package:app/ui/src/nav_items.dart';
+import 'package:app/ui/src/pod.dart';
+import 'package:app/ui/style/style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+const Color _background = Colors.black;
+final Color _backgroundSecondary = Colors.grey[900]!;
+final Color _foreground = Colors.grey[300]!;
+final Color _foregroundSecondary = Colors.grey[400]!;
+
+class GenreListPage extends ConsumerWidget {
+  const GenreListPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<List<GenreIntern>> futureGenres = ref.watch(genrePod);
+
+    return Scaffold(
+        body: CustomScrollView(
+      slivers: [
+        futureGenres.when(
+          data: (genres) => SliverPadding(
+            padding: const EdgeInsets.all(10),
+            sliver: SliverList.separated(
+              itemCount: genres.length,
+              itemBuilder: (context, index) => ListTile(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                leading: Icon(Icons.label),
+                title: Text('${genres[index].name}'),
+                subtitle: Text("${genres[index].count} animes"),
+                tileColor: Theme.of(context).primaryColor,
+                onTap: () async {
+                  await ref
+                      .read(animeQueryPod(HomeNavItem()).notifier)
+                      .set(AnimeQueryIntern()..genresInclude = [genres[index]]);
+                  ref.read(pageGroupPod.notifier).state = AnimeGroup();
+                  ref.read(pageIndexPod.notifier).state = 0;
+                },
+              ),
+              separatorBuilder: (context, index) => const SizedBox(height: 10),
+            ),
+          ),
+          error: (e, _) => const SliverFillRemaining(
+            child: Center(
+              child: TextHeadline("No genres"),
+            ),
+          ),
+          loading: () => const SliverFillRemaining(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+}
