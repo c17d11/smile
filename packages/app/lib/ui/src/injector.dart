50cbb0c58a3d97b13f5980b16ae12399a47f3e66
Mikael Kvist <>
2023-12-10 10:29:20 +0100

    fix: restore injector to as intended

--- packages/app/lib/ui/src/injector.dart
+++ packages/app/lib/ui/src/injector.dart
@@ -1,5 +1,6 @@
 import 'package:app/database/src/database_base.dart';
 import 'package:app/database/src/populate_database.dart';
+import 'package:flutter/foundation.dart';
 import 'package:jikan_api/jikan_api.dart';
 import 'package:kiwi/kiwi.dart';
 
@@ -9,15 +10,15 @@ class Injector {
   void setup() {
     container = KiwiContainer();
 
-    // if (kDebugMode) {
-    //   container.registerSingleton<JikanApi>((container) => JikanApiMockImpl());
-    //   container.registerSingleton((container) => PopulateDatabase(
-    //       container.resolve<Database>(), container.resolve<JikanApi>()));
-    // } else {
-    container.registerSingleton<JikanApi>((container) => JikanApiImpl());
-    container.registerSingleton((container) => PopulateDatabase(
-        container.resolve<Database>(), container.resolve<JikanApi>()));
-    // }
+    if (kDebugMode) {
+      container.registerSingleton<JikanApi>((container) => JikanApiMockImpl());
+      container.registerSingleton((container) => PopulateDatabase(
+          container.resolve<Database>(), container.resolve<JikanApi>()));
+    } else {
+      container.registerSingleton<JikanApi>((container) => JikanApiImpl());
+      container.registerSingleton((container) => PopulateDatabase(
+          container.resolve<Database>(), container.resolve<JikanApi>()));
+    }
 
     container.registerSingleton((container) => Database());
   }
