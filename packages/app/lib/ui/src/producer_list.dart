4565c8275f658ec9af8f618bb88a185cf86a8e98
Mikael Kvist <co17de11@gmail.com>
2024-01-12 23:06:16 +0100

    refactor: extract popup functionality to own function

--- packages/app/lib/ui/src/producer_list.dart
+++ packages/app/lib/ui/src/producer_list.dart
@@ -327,125 +327,10 @@ class ProducerPortrait extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    String s = "";
-    if (producer?.established != null) {
-      String ss = producer!.established!
-          .substring(0, producer!.established!.indexOf('+'));
-      DateTime dt = DateFormat("yyyy-MM-ddTHH:mm:ss").parse(ss);
-      s = DateFormat('yyyy-MM-dd').format(dt);
-    }
-
     return producer == null
         ? buildNull()
         : GestureDetector(
-            onTap: () => showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundColor: Colors.transparent,
-                      child: ClipOval(
-                        child: Image.network(
-                          producer!.imageUrl ?? '',
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    Flexible(
-                      child: Text(
-                        "${producer?.title}",
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.w400,
-                          color: _foreground,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0))),
-                content: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    maxHeight: 600,
-                    maxWidth: 300,
-                  ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const SizedBox(height: 10),
-                        Text(
-                          "ESTABLISHED",
-                          style: TextStyle(
-                            fontSize: 12.0,
-                            fontWeight: FontWeight.w800,
-                            color: _foregroundSecondary,
-                          ),
-                        ),
-                        Text(
-                          s.isNotEmpty ? s : '-',
-                          style: TextStyle(
-                            fontSize: 10.0,
-                            fontWeight: FontWeight.w800,
-                            color: _foregroundThird,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          "ANIMES",
-                          style: TextStyle(
-                            fontSize: 12.0,
-                            fontWeight: FontWeight.w800,
-                            color: _foregroundSecondary,
-                          ),
-                        ),
-                        Text(
-                          producer?.count != null
-                              ? "${producer!.count} animes"
-                              : '-',
-                          style: TextStyle(
-                            fontSize: 10.0,
-                            fontWeight: FontWeight.w800,
-                            color: _foregroundThird,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          "ABOUT",
-                          style: TextStyle(
-                            fontSize: 12.0,
-                            fontWeight: FontWeight.w800,
-                            color: _foregroundSecondary,
-                          ),
-                        ),
-                        Text(
-                          "${producer!.about ?? '-'}",
-                          style: TextStyle(
-                            fontSize: 10.0,
-                            fontWeight: FontWeight.w800,
-                            color: _foregroundThird,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                actions: <Widget>[
-                  TextButton(
-                    child: const Text('OK'),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              ),
-            ),
+            onTap: () => showProducerDetailsPopUp(context, producer!),
             child: Card(
               elevation: 0,
               clipBehavior: Clip.antiAlias,
@@ -534,4 +419,122 @@ class ProducerPortrait extends ConsumerWidget {
             ),
           );
   }
+}
+
+Future showProducerDetailsPopUp(
+    BuildContext context, ProducerIntern producer) async {
+  String s = "";
+  if (producer.established != null) {
+    String ss =
+        producer.established!.substring(0, producer.established!.indexOf('+'));
+    DateTime dt = DateFormat("yyyy-MM-ddTHH:mm:ss").parse(ss);
+    s = DateFormat('yyyy-MM-dd').format(dt);
+  }
+
+  await showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Row(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.transparent,
+            child: ClipOval(
+              child: Image.network(
+                producer!.imageUrl ?? '',
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Flexible(
+            child: Text(
+              "${producer.title}",
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w400,
+                color: _foreground,
+              ),
+            ),
+          ),
+        ],
+      ),
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0))),
+      content: ConstrainedBox(
+        constraints: const BoxConstraints(
+          maxHeight: 600,
+          maxWidth: 300,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 10),
+              Text(
+                "ESTABLISHED",
+                style: TextStyle(
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w800,
+                  color: _foregroundSecondary,
+                ),
+              ),
+              Text(
+                s.isNotEmpty ? s : '-',
+                style: TextStyle(
+                  fontSize: 10.0,
+                  fontWeight: FontWeight.w800,
+                  color: _foregroundThird,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "ANIMES",
+                style: TextStyle(
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w800,
+                  color: _foregroundSecondary,
+                ),
+              ),
+              Text(
+                producer.count != null ? "${producer.count} animes" : '-',
+                style: TextStyle(
+                  fontSize: 10.0,
+                  fontWeight: FontWeight.w800,
+                  color: _foregroundThird,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "ABOUT",
+                style: TextStyle(
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w800,
+                  color: _foregroundSecondary,
+                ),
+              ),
+              Text(
+                producer.about ?? '-',
+                style: TextStyle(
+                  fontSize: 10.0,
+                  fontWeight: FontWeight.w800,
+                  color: _foregroundThird,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('OK'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    ),
+  );
 }
