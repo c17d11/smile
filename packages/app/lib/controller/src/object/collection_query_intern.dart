f94de7a63a28aa7d413acd95f8c3aa9e9f234e65
Mikael Kvist <>
2024-01-28 12:21:20 +0100

    fix: remove correct collection when clicking on button

--- packages/app/lib/controller/src/object/collection_query_intern.dart
+++ packages/app/lib/controller/src/object/collection_query_intern.dart
@@ -2,10 +2,9 @@ class CollectionQueryIntern {
   String? collectionName;
   int? page;
 
-  static CollectionQueryIntern copy(CollectionQueryIntern query) {
-    CollectionQueryIntern c = CollectionQueryIntern();
-    c.collectionName = query.collectionName;
-    c.page = query.page;
-    return c;
+  CollectionQueryIntern copyWith({String? collectionName, int? page}) {
+    return CollectionQueryIntern()
+      ..collectionName = collectionName ?? this.collectionName
+      ..page = page ?? this.page;
   }
 }
