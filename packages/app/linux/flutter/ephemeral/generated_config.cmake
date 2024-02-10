83c5b9d341f6908b1685cd732ed6850103544e66
Mikael Kvist <>
2024-02-10 21:21:27 +0100

    fix: remove weird android 12 image branding

--- packages/app/linux/flutter/ephemeral/generated_config.cmake
+++ packages/app/linux/flutter/ephemeral/generated_config.cmake
@@ -1,21 +1,21 @@
 # Generated code do not commit.
 file(TO_CMAKE_PATH "/home/kuzan/android/flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "/home/kuzan/devs/jikan-ui/packages/app" PROJECT_DIR)
+file(TO_CMAKE_PATH "/home/kuzan/devs/smile/packages/app" PROJECT_DIR)
 
-set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION "1.0.0" PARENT_SCOPE)
 set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
 set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
 set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
-set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
 
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
   "FLUTTER_ROOT=/home/kuzan/android/flutter"
-  "PROJECT_DIR=/home/kuzan/devs/jikan-ui/packages/app"
+  "PROJECT_DIR=/home/kuzan/devs/smile/packages/app"
   "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNzk0Y2YyNjgxYzZjOWZlN2IyNjBlMGU4NGRlOTYyOThkYzljMThiLw=="
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=false"
-  "PACKAGE_CONFIG=/home/kuzan/devs/jikan-ui/packages/app/.dart_tool/package_config.json"
-  "FLUTTER_TARGET=/home/kuzan/devs/jikan-ui/packages/app/lib/main.dart"
+  "PACKAGE_CONFIG=/home/kuzan/devs/smile/packages/app/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/kuzan/devs/smile/packages/app/lib/main.dart"
 )
